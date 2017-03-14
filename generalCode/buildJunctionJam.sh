06c577fa9c2d2ad4be77f5405c61b0638bcfab2c
cbrafter <craigrafter17@gmail.com>
2017-03-14 12:06:43 +0000

    added build file

---
+++ generalCode/buildJunctionJam.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+sudo -H pip install pandas psutil pynput python-twitter profanity
+sudo apt install autoconf build-essential
+sudo apt install libproj-dev proj-bin proj-data  libtool libgdal1-dev libxerces-c3-dev libfox-1.6-0 libfox-1.6-dev
+wget http://prdownloads.sourceforge.net/sumo/sumo-src-0.29.0.zip
+unzip sumo-src-0.29.0.zip
+cd sumo-0.29.0/
+sudo ./configure
+sudo make
+sudo make install
+sumo --version
