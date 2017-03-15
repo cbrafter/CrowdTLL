7919eab1a910b6cc1fd798b2bb1b85b4238bc6f0
cbrafter <craigrafter17@gmail.com>
2017-03-15 09:31:48 +0000

    added update+upgrade to build

--- generalCode/buildJunctionJam.sh
+++ generalCode/buildJunctionJam.sh
@@ -2,6 +2,8 @@
 sudo -H pip install pandas psutil pynput python-twitter profanity
 sudo apt install autoconf build-essential
 sudo apt install libproj-dev proj-bin proj-data  libtool libgdal1-dev libxerces-c3-dev libfox-1.6-0 libfox-1.6-dev
+sudo apt update
+sudo apt -y upgrade
 wget http://prdownloads.sourceforge.net/sumo/sumo-src-0.29.0.zip
 unzip sumo-src-0.29.0.zip
 cd sumo-0.29.0/
@@ -9,3 +11,4 @@ sudo ./configure
 sudo make
 sudo make install
 sumo --version
+# sudo cp /bin/* /usr/bin/
